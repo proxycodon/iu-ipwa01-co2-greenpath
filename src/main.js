92190531ad250c73b2cbefd0b454c0c5b240821c
proxycodon <jmaxwa@gmail.com>
2023-08-29 09:30:17 +0200

    Added Assets + Fonts + minor fixes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import { BootstrapVue } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+const app = createApp(App);
+app.use(BootstrapVue);
+app.mount('#app');
