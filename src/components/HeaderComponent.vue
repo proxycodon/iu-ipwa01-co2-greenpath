b51e3445a376a69fc60dc2b13f76a333813d87fe
proxycodon <jmaxwa@gmail.com>
2023-10-09 14:16:02 +0200

    Adjust header and mobile menu for rtl support

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="header container-fluid">
+  <div :class="['header container-fluid', { 'rtl': isRTL }]">
     <div class="row align-items-center">
-      <div class="col">
+      <div class="col logo-col">
         <router-link to="/">
           <img class="logo" src="@/assets/GP_Logo.svg" alt="Logo">
         </router-link>
-
       </div>
       <div class="col-auto desktop-menu">
         <nav>
@@ -14,26 +13,23 @@
           <router-link to="/our-vision" class="menu-item">Our Vision</router-link>
         </nav>
       </div>
+      <!-- Hamburger menu for mobile view -->
       <div class="col-auto mobile-menu">
         <div class="hamburger" @click="toggleMenu">
           <img :class="{ 'rotated': isMenuOpen }" src="@/assets/hamburger.svg" alt="Menu">
         </div>
       </div>
     </div>
-
     <div :class="['flyout-menu', { 'menu-open': isMenuOpen }]">
       <router-link to="/faq-page" class="menu-item" @click="isMenuOpen = false">FAQ</router-link>
-<router-link to="/contact-us" class="menu-item" @click="isMenuOpen = false">Contact Us</router-link>
-<router-link to="/our-vision" class="menu-item" @click="isMenuOpen = false">Our Vision</router-link>
-
-
+      <router-link to="/contact-us" class="menu-item" @click="isMenuOpen = false">Contact Us</router-link>
+      <router-link to="/our-vision" class="menu-item" @click="isMenuOpen = false">Our Vision</router-link>
       <div class="hamburger" @click="toggleMenu">
         <img :class="{ 'rotated': isMenuOpen }" src="@/assets/hamburger.svg" alt="Menu">
       </div>
     </div>
   </div>
 </template>
-
 
 <script>
 export default {
@@ -42,16 +38,25 @@ export default {
       isMenuOpen: false
     };
   },
-  methods: {
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
+  //check user writing culture and adjust rtl 
+  computed: {
+    isRTL() {
+      const rtlLanguages = ['ar', 'he', 'fa', 'ur'];
+      const userLanguage = navigator.language.split('-')[0];
+      return rtlLanguages.includes(userLanguage);
+    }
   },
-  linkClicked(navigate) {
-    this.toggleMenu();
-    navigate();
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+    linkClicked(navigate) {
+      this.toggleMenu();
+      navigate();
+    }
   }
-}
 };
 </script>
+
 
 <style scoped src="@/assets/styles/header.css"></style>
